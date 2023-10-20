53f8ed65d3b7322dc99187aef515a57c0048bbf2
Maryna-Korbet <97749149+Maryna-Korbet@users.noreply.github.com>
2023-10-21 00:00:53 +0300

    completed task №1

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from './gallery-items.js';
+
+
+const galleryContainer = document.querySelector('.gallery'); 
+
+galleryContainer.insertAdjacentHTML('beforeend', createMarkupGallery(galleryItems));
+galleryContainer.addEventListener('click', onImageClick);
+
+function createMarkupGallery(images) {
+    return images.map(({ preview, original, description }) => 
+        `<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                </li>`
+    ).join('');
+};
+
+function onImageClick(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+    openModalWindow(e);
+};
+
+function openModalWindow(e) {   
+    const bigImageURL = e.target.dataset.source;
+    const modal = basicLightbox.create(`
+        <img src="${bigImageURL}" width="800" height="600">`
+    );
+    modal.show(); 
+
+    window.addEventListener('keydown', e => {
+        if (e.code === 'Escape') {
+        modal.close();
+        }
+    });
+};
+
+
+
+
